12875fb708dd3a27b6866b80442b92e6a0aa28de
Cassandra1245 <guillermogarciasuarez@gmail.com>
2024-11-24 14:26:56 +0100

    ProyectoCompletado

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
+import React, { useState } from 'react';
+import { Incrementar, Decrementar } from './variaciones';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [numero, setNumero] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="Ap-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>El numero: {numero}</p>
+        <button onClick={() => Incrementar(numero, setNumero)}>Incrementar</button>
+        <button onClick={() => Decrementar(numero, setNumero)}>Decrementar</button>
       </header>
     </div>
   );
---
+++ src/variaciones.js
@@ -0,0 +1,8 @@
+export function Incrementar(numero, setNumero) {
+    setNumero(numero + 1);
+}
+
+export function Decrementar(numero, setNumero) {
+    setNumero(numero - 1);
+}
+
